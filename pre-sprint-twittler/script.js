7e1b2bc860535cac63ca0583d472951cc83774d6
Seungyeon Yoo <y2s9310@gmail.com>
2021-02-05 02:42:29 +0900

    Add function for changing time format

--- pre-sprint-twittler/script.js
+++ pre-sprint-twittler/script.js
@@ -4,18 +4,48 @@ const btnNewTweet = document.querySelector('.btn-register');
 const userName = document.querySelector('#username');
 const newTweetContent = document.querySelector('#newTweetContent');
 const refresh = document.querySelector('.refresh');
+const readingArea = document.querySelector('#tweets');
 
+// ----작성시간을 입력받아 작성한 시점으로부터 현재시간까지 얼마나 지났는지를 반환해주는 함수입니다.-----------
+function createdTimeToFormatted(time) {
+    let now = new Date(); // 현재 날짜 정보를 now 객체로 만듭니다.
+    let createdTime = new Date(time); // 작성날짜 정보를 인자로 받아와 작성날짜에 대한 정보를 createdTime 객체로 만듭니다
+	let year = createdTime.getFullYear().toString();
+  	let month = (createdTime.getMonth() + 1).toString().padStart(2, '0');
+  	let date = createdTime.getDate().toString().padStart(2, '0');
+    // 트윗을 작성한 시각부터 현재까지의 시간을 계산하기 위해 날짜정보를 밀리초로 가져오는 getTime() 함수를 사용합시다.
+    let toNow = now.getTime(); // 현재 날짜 정보를 밀리초로 바꿈
+    let toCreated = createdTime.getTime(); // 작성한 날짜 정보를 밀리초로 바꿈
+    let passedTime = toNow - toCreated; // 트윗을 작성한 시점부터 현재까지 흐른 시간(밀리초로 바꿈)
+    let passedSec = Math.round(passedTime / 1000);
+    let passedMin = Math.round(passedTime / (1000 * 60));
+    let passedHour = Math.round(passedTime / (1000 * 60 * 60));
+    let passedDay = Math.round(passedTime / (1000 * 60 * 60 * 24));
+    // 화면에 표시할 기준을 잡아봅시다.
+    // 1분 미만이면 '방금'
+    // 1분 이상 1시간 미만이면 `${min}분 전`
+    // 1시간 이상 1일 미만이면 `${hour}시간 전`
+    // 1일 이상일 경우 `${yyyy}.${mm}.${dd}` 으로 표시해줍시다.
+    if (passedDay > 0) {
+        return `${year}.${month}.${date}`;
+    } else if (passedHour > 0) {
+        return `${passedHour}시간 전`;
+    } else if (passedMin > 0) {
+        return `${passedMin}분 전`;
+    } else {
+        return '방금';
+    }
+}
 
 // ----------------------------------------기존의 트윗을 보여줍니다.----------------------
-const readingArea = document.querySelector('#tweets');
-function printTweets(){
+function printTweets() {
     DATA.forEach(printTweet);
 }
-function printTweet(DATA){
+function printTweet(DATA) {
     let tweetElement = makeTweetElement(DATA);
     readingArea.prepend(tweetElement);
 }
-function makeTweetElement(DATA){
+function makeTweetElement(DATA) {
     //tweet li --
     let liElement = document.createElement('li');
     liElement.classList.add('tweet', 'row', 'pl-15', 'pr-15', 'pt-10');
@@ -43,7 +73,7 @@ function makeTweetElement(DATA){
     //time span --
     let timeElement = document.createElement('span');
     timeElement.classList.add('time');
-    timeElement.textContent = DATA.created_at;
+    timeElement.textContent = createdTimeToFormatted(DATA.created_at);
     twtInfoElement.appendChild(timeElement);
     //content p (트윗 내용) --
     let contentElement = document.createElement('p');
@@ -76,7 +106,7 @@ function makeNewTweetElement() {
     // 그 객체를 DATA에 unshift하자
     DATA.push(newTweet);
     // 추가된 트윗을 보여주자
-    printTweet(DATA[DATA.length-1]);
+    printTweet(DATA[DATA.length - 1]);
     // 입력하는 칸을 비우자
     userName.value = '';
     newTweetContent.value = '';
@@ -95,11 +125,11 @@ readingArea.addEventListener('click', function (event) {
 
     if (target.classList[0] === 'username') {
         // printTweets을 실행하되, target.textContent가 DATA.user === target.textContent
-        let tweetList = DATA.filter(function(tweet){
+        let tweetList = DATA.filter(function (tweet) {
             return tweet.user === target.textContent;
         });
         console.log(tweetList)
-    
+
         //readingArea.innerHTML = '';
         while (readingArea.firstChild) {
             readingArea.removeChild(readingArea.firstChild);
